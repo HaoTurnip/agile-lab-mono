d6a40a2cb9bbf5779c63d5072a8215237155c572
Turnip <connection5bars@gmail.com>
2024-11-01 20:09:10 +0200

    commit from cli

---
+++ 21P0109_AHMEDMOHAMEDELSHEIKH_MAIN.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+using namespace std;
+
+int add(int a, int b){
+
+    return a+b;
+}
+
+int main() {
+    int a = 5;
+    int b = 6;
+    cout << add(a,b) << endl;
+    return 0;
+}
